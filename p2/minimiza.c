1214a27d6bfa72c0dca43d8baf07c8f28b4404df
martagm1 <martagm_97@hotmail.com>
2019-12-14 14:37:19 +0100

    somos unas cracks pero aun queda mazo gg

--- p2/minimiza.c
+++ p2/minimiza.c
@@ -1,10 +1,11 @@
 
 #include "minimiza.h"
 
-AFND * AFNDMinimiza(AFND * afnd){
+AFND * AFNDMinimiza(AFND* afnd){
   int num_simbolos = AFNDNumSimbolos(afnd);
   int num_estados = AFNDNumEstados(afnd);
   int* vistos = (int*)malloc(num_estados*sizeof(int));
+  int* visitados = NULL;
   int estado_ini = AFNDIndiceEstadoInicial(afnd);
   int i;
   int n = 0;
@@ -17,6 +18,7 @@ AFND * AFNDMinimiza(AFND * afnd){
   for (i = 0; i < num_estados; i++){
     vistos[i] = 0;
   }
+
   /*Buscamos los estados que necesitamos para el AFD mediante busqueda en anchura*/
   vistos = bfs(estado_ini, num_simbolos, num_estados, vistos, afnd);
   printf("\nVistosss\n");
@@ -24,6 +26,8 @@ AFND * AFNDMinimiza(AFND * afnd){
     printf("%d ", vistos[i]);
     if (vistos[i] == 1){
       n++;
+      visitados = realloc(visitados, n*sizeof(int));
+      visitados[n-1] = i;
     }
   }
   printf("\nHay %d estados accesibles de %d\n", n, num_estados);
@@ -32,7 +36,15 @@ AFND * AFNDMinimiza(AFND * afnd){
   matriz = ini_matriz(n);
 
   /*Cargamos la matriz con el algoritmo propuesto en clase*/
+  matriz = estados_equivalentes(visitados, afnd, matriz, n);
 
+  /*Liberamos memoria final*/
+  free(vistos);
+  free(visitados);
+  for (i = 0; i < (n-1); i++){
+    free(matriz[i]);
+  }
+  free(matriz);
   return NULL;
 }
 
@@ -44,18 +56,20 @@ int* bfs(int estado, int num_simbolos, int num_estados, int* vistos, AFND* afnd)
   for(i = 0; i < num_estados; i++){
     if(trans[i]){
       estado = i;
-      if(vistos[i] == 0){
-        bfs(estado, num_simbolos, num_estados, vistos, afnd);
+      if(vistos[estado] == 0){
+        vistos = bfs(estado, num_simbolos, num_estados, vistos, afnd);
       }
     }
   }
-return vistos;
+  free(trans);
+  return vistos;
 }
 
 int* transiciones (int est, int num_estados, int num_simbolos, AFND* afnd){
   int i, j;
   int* transiciones = (int*)malloc(num_estados*sizeof(int));
-  for (i = 0; i<num_estados; i++){
+  for (i = 0; i < num_estados; i++){
+    transiciones[i] = 0;
     if (AFNDCierreLTransicionIJ(afnd, est, i)){
       transiciones[i] = 1;
     }
@@ -63,9 +77,6 @@ int* transiciones (int est, int num_estados, int num_simbolos, AFND* afnd){
       for (j = 0; j < num_simbolos; j++){
         if (AFNDTransicionIndicesEstadoiSimboloEstadof(afnd, est, j, i)){
           transiciones[i] = 1;
-        }
-        else{
-          transiciones[i] = 0;
         }
       }
     }
@@ -77,10 +88,70 @@ int** ini_matriz(int n){
   int i,j;
   int** matriz = (int **)malloc((n-1)*sizeof(int*));
   for (i = 0; i < (n-1); i++){
-    matriz[i] = (int *)malloc(n*sizeof(int));
-    for (j = 0; j < n; j++){
+    matriz[i] = (int *)malloc((n-1)*sizeof(int));
+    for (j = 0; j < n-1; j++){
       matriz[i][j] = 0;
     }
   }
   return matriz;
+}
+
+/* Marca los estados no equivalentes en la matriz*/
+int** estados_equivalentes(int* visitados, AFND* afnd, int** matriz, int n){
+  int* finales = NULL;
+  int i, estado;
+  int estados_fin = 0;
+
+  printf("Estados finaless\n");
+  for (i = 0; i < n; i++){
+    estado = visitados[i];
+    if (AFNDTipoEstadoEn(afnd, estado) == FINAL || AFNDTipoEstadoEn(afnd, estado) == INICIAL_Y_FINAL){
+      estados_fin ++;
+      finales = realloc(finales, estados_fin*sizeof(int));
+      finales[estados_fin-1] = estado;
+    }
+  }
+  /*Distinguimos los estados finales*/
+  matriz = marcar_finales(visitados, matriz, finales, n-1, estados_fin);
+
+  /*Busqueda de equivalentes por transiciones*/
+  for (i = 0; i < estados_fin; i++){
+    matriz = marcar(visitados, matriz, n, i);
+  }
+
+  free(finales);
+  return matriz;
+}
+
+/* Marcamos como distinguibles todas las relaciones con el final */
+int** marcar_finales(int* visitados, int** matriz, int* finales, int tam_matriz, int estados_fin){
+  int i, j, indice;
+  for (i = 0; i < estados_fin; i++){
+    indice = finales[i];
+    for (j = 0; j <= tam_matriz; j ++){
+      if (indice < j ){
+        matriz[j-1][indice] = 1;
+        printf("ponemos un 1 en j=%d  en indice=%d\n", j, indice);
+      }
+      else if (j < indice){
+        matriz[indice-1][j] = 1;
+        printf("ponemos un 1 en indice=%d  en j=%d\n",indice, j);
+      }
+    }
+  }
+
+  for (i = 0; i < tam_matriz; i++){
+    printf("\n");
+    for (j = 0; j < tam_matriz; j++){
+      printf("%d", matriz[i][j]);
+    }
+  }
+  printf("\n");
+
+  return matriz;
+}
+
+/*Marca el resto de estados distinguiendo en clases -- recursiva*/
+int** marcar(int* visitados, int** matriz, int n){
+
 }
--- p2/minimiza.h
+++ p2/minimiza.h
@@ -12,4 +12,7 @@ AFND * AFNDMinimiza(AFND * afd);
 int* bfs(int estado, int num_simbolos, int num_estados, int* vistos, AFND* afd);
 int* transiciones (int est, int num_estados, int num_simbolos, AFND* afd);
 int** ini_matriz(int n);
+int** estados_equivalentes(int* visitados, AFND* afnd, int** matriz, int n);
+int** marcar_finales(int* visitados, int** matriz, int* finales, int n, int estados_fin);
+int** marcar(int* visitados, int** matriz, int n);
 #endif
